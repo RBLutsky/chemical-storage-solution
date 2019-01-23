9628cd9c0bda3eb3c0605b7f4321bb1f4cedfcc3
Rhys Lutsky <rhyslutsky@gmail.com>
2019-01-23 17:56:34 -0500

    init commit front end, react installed and files started

---
+++ client/src/Components/Category.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Component } from 'react';
+
+class Category extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            category: '',
+            definition: '',
+            cautions: ''
+        };
+    }
+
+    render() {
+        return (
+            //SOME JSX TO DISPLAY
+        );    
+    }
+}
