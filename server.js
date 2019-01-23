737e0e4cbaaea2f8ffe03a9f713c8990794c8588
Rhys Lutsky <rhyslutsky@gmail.com>
2019-01-23 10:11:05 -0500

    GET chemical by name with PostMan

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql');
-
-//connect to MySQL Database with mysql node pkg
-const connection = mysql.createConnection({
+var mysql = require('mysql');
+//our application creates a group of connections that will stay open to the database and then will use and reuse the connections whenever they become free
+var pool = mysql.createPool({ 
+//connect to MySQL Database with mysql node pkg.  This is a shortcut for the pool.getConnection() -> connection.query() -> connection.release() code flow
+    connectionLimit: 10,
     host: 'localhost',
-    user: 'root',
+    user: 'user',
     password: 'password',
     database: 'mydb',
     // port: 3306
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.log('Error connecting to Db' + err.stack);
-        return;
-    }
-    console.log('Connection established');
-});
+
+// pool.connect((err) => {
+//     if (err) {
+//         console.log('Error connecting to Db' + err.stack);
+//         return;
+//     }
+//     console.log('Connection established');
+// });
 
 
 //If using db as self instead of root, add to Workbench query and execute: ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password'
@@ -27,25 +29,29 @@ connection.connect((err) => {
 
 
 //Search for a chemical in the chemical table by name that user input
-connection.query(`SELECT * FROM chemical WHERE chemical_name LIKE 'hydrochloric%'`,  (error, results, fields)=> {
-    if (error) throw error;
-console.log('results', results);
-});
+app.get('/chemical', (req, res) => {
+    pool.query(`SELECT * FROM chemical WHERE chemical_name LIKE 'hydrochloric%'`,  (error, results, fields)=> {
+        if (error) throw error;
+        res.json(results);
+    // console.log('results', results);
+    });
+})
+
 
 //Select a specific chemical from the chemical table by id (user click on name)
-connection.query(`SELECT * FROM chemical WHERE id = 417`, (error, results, fields) => {
-    if (error) throw error;
-    console.log('results', results);
-});
+// pool.query(`SELECT * FROM chemical WHERE id = 417`, (error, results, fields) => {
+//     if (error) throw error;
+//     console.log('results', results);
+// });
 
-connection.end((err) => {
-    // The connection is terminated gracefully
-    // Ensures all previously enqueued queries are still
-    // before sending a COM_QUIT packet to the MySQL server.
-});
+// connection.end((err) => {
+//     // The connection is terminated gracefully
+//     // Ensures all previously enqueued queries are still
+//     // before sending a COM_QUIT packet to the MySQL server.
+// });
 
 
-// app.listen(7000
-//     , () => {
-//     console.log("None.js is listening on port" + 7000)
-// })
+app.listen(7000
+    , () => {
+    console.log("None.js is listening on port" + 7000)
+})
