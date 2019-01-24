21dedad716f1b4568918402698b61738e8464023
Rhys Lutsky <rhyslutsky@gmail.com>
2019-01-24 12:49:03 -0500

    search and click search button

--- client/src/Components/ChemicalPage.js
+++ client/src/Components/ChemicalPage.js
@@ -1,42 +1,50 @@
 //PAGE = SEARCH BY CHEMICAL NAME
-
-//search for chemcial by name and return list of matches
-//click to add to inventory
+//includes: 
+    //search for chemcial by name
+    //return list of matches
+    //click to add to inventory
 //button to HomePage
 //button to Inventory
 
 import React from 'react';
-// import { Component } from 'react';
+import { Component } from 'react';
 import SearchBar from './SearchBar';
-import { Button} from 'reactstrap';
+import { Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
+class ChemicalPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            chemicals: [],
+        };
+        this.chemicalSearch = this.chemicalSearch.bind(this);
+    }
 
-const ChemicalPage = () => {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         chemicalName: '',
-    //         storageCategory: '',
-    //     };
-    // }
+    //search for a chemical
+    chemicalSearch(searchTerm) {
+        console.log('the chemicalPage:' ,searchTerm);
 
-    // render() {
-    return (
-        <div>
-            <h1>Search by Chemical Name</h1>
-            <SearchBar />
-            <Chemical-SearchList />
-            <Link to='/'>
-                <Button color="primary">Home</Button>
-            </Link>
-            <Link to='/inventory'>
-                <Button color="primary">View Inventory</Button>
-            </Link>
-        </div>
-    )
+    }
 
+    render() {
+        return (
+            <div>
+                <h1>Search by Chemical Name</h1>
+
+                {/* set new prop/value(function) to pass to SearchBar */}
+                <SearchBar onSearchTermChange={this.chemicalSearch} />
+                <Chemical-SearchList />
+                <Link to='/'>
+                    <Button color="primary">Home</Button>
+                </Link>
+                <Link to='/inventory'>
+                    <Button color="primary">View Inventory</Button>
+                </Link>
+            </div>
+        )
+
+    }
 }
-// }
 
-export default ChemicalPage
+    export default ChemicalPage
--- client/src/Components/SearchBar.js
+++ client/src/Components/SearchBar.js
@@ -1,29 +1,48 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Button } from 'reactstrap';
 
 class SearchBar extends Component {
     constructor(props) {
         super(props);
-        this.state = { searchTerm: "" };
+        this.state = {
+            searchTerm: ""
+        };
+
+        this.onChange = this.onChange.bind(this);
+        this.onClick = this.onClick.bind(this);
+    }
+    // grab the term each time the input changes
+    onChange(e) {
+        this.setState({ searchTerm: e.target.value });
+        this.props.onSearchTermChange(this.searchTerm);
+       
+    }
+
+    //go get the data - should be getting in Chemical page?
+    onClick(e) {
+        alert("A chemical was submitted: " + this.state.searchTerm);
+        e.preventDefault();
     }
 
     render() {
         return (
-            <div className="search-bar">
+            <div className="search-bar" >
                 <input
                     //To update, we need to bind the value of the input to our term
+                    type='search'
                     value={this.state.searchTerm}
-
-                    onChange={event => this.onInputChange(event.target.value)} />
+                    name='q'
+                    placeholder="chemical name"
+                    onChange={this.onChange}
+                    
+                />
+                <span> 
+                    <Button type='submit' onClick={this.onClick}color="primary">Search</Button>
+                </span>
             </div>
         );
     }
-    // we want to update the term each time the input changes
-    onInputChange(searchTerm) {
-        this.setState({ searchTerm });
-        // console.log(this.state);
-        // invoke props.onSearchTermChange and pass in the term every time the input changes
-        this.props.onSearchTermChange(searchTerm);
-    }
+
 }
 
 export default SearchBar;
